80fd538e24b8e9e90e13e9a76c13cc107c30e306
rlduswl <127376532+rlduswl@users.noreply.github.com>
2025-03-27 23:26:54 +0900

    Add files via upload

---
+++ 0321/TextViewTest2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.textviewtest2;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv1, tv2, tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+
+        tv1.setText("자바 코드로 변경하였습니다.");
+        tv2.setTextColor(Color.BLUE);
+        tv2.setTextSize(60);
+        tv3.setTextSize(60);
+        tv3.setTypeface(Typeface.SERIF, Typeface.ITALIC);
+    }
+}
